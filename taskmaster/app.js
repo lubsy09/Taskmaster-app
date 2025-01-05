55f32f55208a098faff1da36dd6510cf820cb373
lubsy09 <lubabatuahmad60@gmail.com>
2025-01-05 15:40:03 +0100

    Added route to serve Home page

--- taskmaster/app.js
+++ taskmaster/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const authRoutes = require('./Routes/authRoutes.js');
 const taskRoutes = require('./Routes/taskRoutes.js');
@@ -10,11 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 app.use('/api/users', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
 // Test Route
-app.get('/', (req, res) => res.send('TaskMaster API is running'));
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public/home.html'));
+});
 
 module.exports = app; 
